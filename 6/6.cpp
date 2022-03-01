db899c68f4df4cf530da37f3a21031fd82ef8b8a
Shutova Anna <90278446+DangerNoodle557@users.noreply.github.com>
2022-03-01 15:16:59 +0500

    Create 6.cpp

---
+++ 6/6.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool ch(string word)
+{
+ int len = word.length();
+ for(int i = 0; i < len/2; ++i)
+ {
+  if(word[i] != word[len-i-1])
+  {
+   return false;
+  }
+ }
+ return true;
+}
+
+int main()
+{
+ setlocale(LC_ALL, "Russian"); 
+ string str;
+ cout « "введите слово: ";
+ cin » str;
+ if(ch(str))
+ {
+  cout « "слово является палиндромом ";
+ }
+ else
+ {
+  cout « "слово не является палиндромом";
+ }
+ return 0;
+}
